e6e9bb02647a14a057f0f04f4648ea7bf643aade
chenbainan <chenbainan@teamax-cn.com>
2020-09-22 12:00:31 +0800

    add test for third page screams

--- src/ScreamSln/Test/GetScreamsTest.cs
+++ src/ScreamSln/Test/GetScreamsTest.cs
@@ -38,6 +38,9 @@ namespace Test
             Assert.Equal(screams.List.Count, SIZE);
         }
 
+        /// <summary>
+        /// Test for second page
+        /// </summary>
         [Fact]
         public async void GetScreams_SecondPageIndexAndSize_ReturnPartOfDataOfPage()
         {
@@ -46,6 +49,34 @@ namespace Test
             const int SIZE = 10;
             const int TOTAL_PAGE = 2;
             const int RETURN_SIZE = 2;
+
+            Screams.IScreamsManager screamsManager = new Screams.DefaultScreamsManager
+            (
+                _db, redisConn
+            );
+
+            //  act
+            var screams = await screamsManager.GetScreamsAsync(INDEX, SIZE);
+
+            //  assert
+            Assert.Equal(screams.Index, INDEX);
+            Assert.Equal(screams.Size, SIZE);
+            Assert.Equal(screams.TotalSize, ScreamModels.Count);
+            Assert.Equal(screams.TotalPage, TOTAL_PAGE);
+            Assert.Equal(screams.List.Count, RETURN_SIZE);
+        }
+
+        /// <summary>
+        /// Test third page
+        /// </summary>
+        [Fact]
+        public async void GetScreams_ThirdPageIndexAndSize_ReturnEmptyList()
+        {
+            //  arrange
+            const int INDEX = 3;
+            const int SIZE = 10;
+            const int TOTAL_PAGE = 2;
+            const int RETURN_SIZE = 0;
 
             Screams.IScreamsManager screamsManager = new Screams.DefaultScreamsManager
             (
