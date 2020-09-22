18fd7ce2d8cb06f0897e463f833af3e7c43ec7fd
chenbainan <chenbainan@teamax-cn.com>
2020-09-22 17:23:31 +0800

    add test for scream

---
+++ src/ScreamSln/Test/GetScreamSingleTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Test
+{
+    public class GetScreamSingleTest : DBSeedFactory
+    {
+
+        [Fact]
+        public async void GetScream_ExistId_ReturnScream()
+        {
+            //  arrange
+            const int EXIST_ID = 1;
+
+            Screams.IScreamsManager screamsManager = new Screams.DefaultScreamsManager
+            (
+                _db, redisConn
+            );
+
+            //  act
+            var scream = await screamsManager.GetScreamAsync(EXIST_ID);
+
+            //  assert
+            Assert.NotNull(scream);
+        }
+
+        [Fact]
+        public async void GetScream_NotExistId_ReturnNull()
+        {
+            //  arrange
+            const int NOT_EXIST_ID = 10111;
+
+            Screams.IScreamsManager screamsManager = new Screams.DefaultScreamsManager
+            (
+                _db, redisConn
+            );
+
+            //  act
+            var scream = await screamsManager.GetScreamAsync(NOT_EXIST_ID);
+
+            //  assert
+            Assert.Null(scream);
+        }
+
+        [Fact]
+        public async void GetScream_InvalidId_ReturnNull()
+        {
+            //  arrange
+            const int INVALID_ID = -1;
+
+            Screams.IScreamsManager screamsManager = new Screams.DefaultScreamsManager
+            (
+                _db, redisConn
+            );
+
+            //  act
+            var scream = await screamsManager.GetScreamAsync(INVALID_ID);
+
+            //  assert
+            Assert.Null(scream);
+        }
+    }
+}
